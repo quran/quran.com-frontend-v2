cc3f0760b7b65293b1339baab092f238209ef178
Naveed Ahmad <naveedahmada036@gmail.com>
2019-02-17 00:42:31 +0500

    added more locals

--- config/initializers/translation.rb
+++ config/initializers/translation.rb
@@ -1,7 +1,7 @@
 TranslationIO.configure do |config|
   config.api_key        = Rails.application.credentials[:translation_key]
   config.source_locale  = 'en'
-  config.target_locales = ['ar', 'ur-PK']
+  config.target_locales = ['sq', 'ar', 'nl', 'fr', 'id', 'it', 'fa', 'tr', 'ur']
 
   # Uncomment this if you don't want to use gettext
   # config.disable_gettext = true
