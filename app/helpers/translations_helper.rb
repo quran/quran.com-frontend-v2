2cc144e9c3979dacde06846a6659507f0a2a0d6c
Naveed Ahmad <naveedahmada036@gmail.com>
2019-03-04 05:20:46 +0500

    updated locales

---
+++ app/helpers/translations_helper.rb
@@ -0,0 +1,13 @@
+module TranslationsHelper
+  def _t(key)
+    "<span class='#{I18n.locale}'>#{t key}</span>".html_safe
+  end
+
+  def available_locales
+    locales = I18n.backend.send(:translations)
+
+    locales.keys.map do |key|
+      [key, locales[key][:local_native_name]]
+    end
+  end
+end
