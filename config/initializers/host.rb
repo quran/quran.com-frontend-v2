bc72435619f52e6eeb083546262492abb26e9d63
Naveed Ahmad <naveedahmada036@gmail.com>
2019-04-19 02:56:55 +0500

    added beta

--- config/initializers/host.rb
+++ config/initializers/host.rb
@@ -3,4 +3,5 @@
 Rails.application.config.hosts << 'staging.quran.com'
 Rails.application.config.hosts << 'www.quran.com'
 Rails.application.config.hosts << 'www.qurancdn.com'
+Rails.application.config.hosts << 'beta.quran.com'
 Rails.application.config.hosts << 'localhost'
