e8913dab42785645fa58b39c799414591a587ad1
Naveed Ahmad <naveedahmada036@gmail.com>
2019-04-21 05:44:20 +0500

    added qurancdn in hosts

--- config/initializers/host.rb
+++ config/initializers/host.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.hosts += ["staging.quran.com",
+"qurancdn.com",
                                    "beta.quran.com",
                                    "beta.qurancdn.com",
                                    "beta.qurancdn.com",
