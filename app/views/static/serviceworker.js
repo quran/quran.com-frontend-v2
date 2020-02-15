159f710040750d3970a3d2184930693173e89943
Naveed Ahmad <naveedahmada036@gmail.com>
2020-02-15 10:17:51 +0500

    removed bootstrap cdn, we've packed it already

--- app/views/static/serviceworker.js
+++ app/views/static/serviceworker.js
@@ -1,64 +1,74 @@
 importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
 if (workbox) {
-    workbox.googleAnalytics.initialize();
-    // Enable navigation preload.
-    workbox.navigationPreload.enable();
+  workbox.googleAnalytics.initialize();
+  // Enable navigation preload.
+  // workbox.navigationPreload.enable();
 
-    workbox.routing.registerRoute(
-        new RegExp(
-            "^https://(cdnjs|fonts|oss|maxcdn|cdn|audio).(?:googleapis|gstatic|cloudflare|maxcdn|bootstrapcdn|ravenjs|qurancdn|quran).com/(.*)"
-        ),
-        new workbox.strategies.CacheFirst({
-            cacheName: "quran-cdn",
-            plugins: [
-                new workbox.cacheableResponse.Plugin({
-                    statuses: [0, 200]
-                }),
-                new workbox.expiration.Plugin({
-                    maxAgeSeconds: 12 * 30 * 24 * 60 * 60, //one year
-                    maxEntries: 50
-                })
-            ]
+  workbox.routing.registerRoute(
+    new RegExp(
+      "^https://(cdnjs|fonts|oss|maxcdn|cdn|audio).(?:googleapis|gstatic|cloudflare|maxcdn|bootstrapcdn|ravenjs|qurancdn|quran).com/(.*)"
+    ),
+    new workbox.strategies.CacheFirst({
+      cacheName: "quran-cdn",
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 12 * 30 * 24 * 60 * 60, //one year
+          maxEntries: 50
         })
-    );
+      ]
+    })
+  );
 
-    workbox.routing.registerRoute(
-        /\.(?:js|css|eot|ttf|woff|woff2|otf)$/,
+  workbox.routing.registerRoute(
+    /\.(?:js|css|eot|ttf|woff|woff2|otf)$/,
 
-        new workbox.strategies.CacheFirst({
-            cacheName: "quran-static",
-            plugins: [
-                new workbox.cacheableResponse.Plugin({
-                    statuses: [0, 200]
-                }),
-                new workbox.expiration.Plugin({
-                    maxAgeSeconds: 12 * 30 * 24 * 60 * 60, //one year
-                    maxEntries: 50
-                })
-            ]
+    new workbox.strategies.CacheFirst({
+      cacheName: "quran-static",
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 12 * 30 * 24 * 60 * 60, //one year
+          maxEntries: 50
         })
-    );
+      ]
+    })
+  );
 
-    workbox.routing.registerRoute(
-        /\.(?:png|gif|jpg|jpeg|svg|mp3|ogg)$/,
-        new workbox.strategies.CacheFirst({
-            cacheName: "images",
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 200,
-                    maxAgeSeconds: 12 * 30 * 24 * 60 * 60 // 1 year
-                })
-            ]
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg|mp3|ogg)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: "images",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 200,
+          maxAgeSeconds: 12 * 30 * 24 * 60 * 60 // 1 year
         })
-    );
+      ]
+    })
+  );
 
-    workbox.routing.registerRoute(
-        new RegExp("^https://(www|beta)?.(?:quran).com/(.*)")
-        );
-    console.log("Workbox is ready");
+  workbox.routing.registerRoute(
+    new RegExp("^https://(www|beta)?.(?:quran).com/(.*)"),
+
+    new workbox.strategies.NetworkFirst({
+      cacheName: "quran-pwa",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 200,
+          maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+        })
+      ]
+    })
+  );
+  console.log("Workbox is ready");
 } else {
-    console.log("Boo! Workbox didn't load 😬");
+  console.log("Boo! Workbox didn't load 😬");
 }
