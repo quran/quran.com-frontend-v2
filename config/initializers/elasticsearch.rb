2a0ebe7358aa2b0cda797629eefeaca1ca75a745
Naveed Ahmad <naveedahmada036@gmail.com>
2020-04-19 01:00:49 +0500

    changed the port

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,13 +1,7 @@
 options = if ENV['ELASTICSEARCH_HOST']
             {host: ENV['ELASTICSEARCH_HOST']}
           else
-            {
-                host: 'staging.quran.com',
-                port: 32769,
-                adapter: :typhoeus,
-                scheme: :http
+            {host: 'staging.quran.com:32772'}
+          end.merge(adapter: :typhoeus)
 
-            }
-          end
-
-Elasticsearch::Model.client = Elasticsearch::Client.new(options)
+Elasticsearch::Model.client = Elasticsearch::Client.new options
