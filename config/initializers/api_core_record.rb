1be94418454a7b27004ee7dfd63a6115be9a6dc9
Naveed Ahmad <naveedahmada036@gmail.com>
2020-04-22 04:53:14 +0500

    removed quran-core connection

--- config/initializers/api_core_record.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-#require 'quran_core'
-
-#class ApiCoreRecord < ActiveRecord::Base
- # establish_connection Rails.env.production? ? :quran_core : :quran_core_development
-#end
