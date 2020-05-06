873675dc639cba271e663a29e94aa2f9bd00881a
Naveed Ahmad <naveedahmada036@gmail.com>
2020-05-07 02:44:07 +0500

    upgraded jquery to 3.5.1 to fix Bootstrap's collapse twbs/bootstrap#30553

--- app/helpers/lazy_tag_helper.rb
+++ app/helpers/lazy_tag_helper.rb
@@ -2,6 +2,9 @@ module LazyTagHelper
   DEFAULT_IMG_DATA = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
 
   def lazy_image_tag(name, **options)
-    image_tag DEFAULT_IMG_DATA, class: "#{options[:class]} lazyload", data: { src: resolve_path_to_image(name) }
+    options[:class] = "#{options[:class]} lazyload"
+    options[:data] =  { src: resolve_path_to_image(name) }
+
+    image_tag DEFAULT_IMG_DATA, options
   end
 end
