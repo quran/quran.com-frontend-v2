47f2f1f8b296a3c7d968f667d4544c9ab4a688c4
Naveed Ahmad <naveedahmada036@gmail.com>
2020-06-06 01:59:19 +0500

    changed ES host env variable

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,7 @@
-options = if ENV['ELASTICSEARCH_HOST']
-            {host: ENV['ELASTICSEARCH_HOST']}
+options = if ENV['ELASTICSEARCH_PORT_9200_TCP_ADDR']
+            {host: ENV['ELASTICSEARCH_PORT_9200_TCP_ADDR']}
           else
-            {host: 'staging.quran.com:32772'}
+            {}
           end.merge(adapter: :typhoeus)
 
 Elasticsearch::Model.client = Elasticsearch::Client.new options
