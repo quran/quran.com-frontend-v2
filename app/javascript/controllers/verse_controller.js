430d48ce5370aec4ea34c7137f1c65f037d46a12
umar482 <umar.omtomo@gmail.com>
2020-09-03 20:00:18 +0500

    Fix translation space issue

--- app/javascript/controllers/verse_controller.js
+++ app/javascript/controllers/verse_controller.js
@@ -54,7 +54,7 @@ export default class extends Controller {
   connect() {
     this.el = $(this.element);
 
-    if (this.el.find(".arabic").height() + this.el.find(".translation").height() < 300) {
+    if (this.el.find(".arabic").height() + this.el.find(".translation").height() < 20) {
       this.actionsTarget.classList.add("col-md-12", "order-md-1")
       this.actionsTarget.children[0].classList.remove("flex-md-column")
       this.actionsTarget.children[0].classList.add("justify-content-md-evenly")
