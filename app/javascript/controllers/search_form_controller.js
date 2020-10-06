a21c233473a513bc14f3b5fc65814bcbeb0fb9bb
Naveed Ahmad <naveedahmada036@gmail.com>
2020-10-06 12:05:47 +0500

    improved search

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -1,5 +1,5 @@
-import { Controller } from "stimulus";
-import { debounce } from "lodash-es";
+import {Controller} from "stimulus";
+import {debounce} from "lodash-es";
 import DeviceDetector from "../utility/deviceDetector";
 
 const FILTER_DELAY = 150;
@@ -11,37 +11,54 @@ export default class extends Controller {
     this.searchBox = $(el).find("[name=query]");
     this.device = new DeviceDetector();
 
+    this.bindSuggestion();
+    this.resizeHandler();
+
+    window.addEventListener("resize", () => this.resizeHandler());
+
+    this.element
+      .querySelector("#search-trigger")
+      .addEventListener("click", e => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        let mobile = this.device.isMobile();
+        let query = this.searchBox.val();
+
+        if (mobile && 0 == query.length) {
+          el.classList.toggle("expand-collapse");
+          el.classList.toggle("sb-search-open");
+        }
+
+        if (query.length > 0) {
+          this.doSearch(query);
+        }
+      });
+
+    document.addEventListener('click', (e) => {
+      if (this.element.contains(e.target)) {
+        $(".suggestions").show();
+      } else {
+        $(".suggestions").hide()
+      }
+    })
+
+    $(".search-btn").click(() => {
+      alert('s')
+    })
+  }
+
+  bindSuggestion() {
     this.searchBox
       .change(e => {
         this.getSuggestions(e.target.value);
       })
       .keyup(
-        debounce(function(e) {
+        debounce(function (e) {
           /* fire the above change event after every letter is typed with a delay of 250ms */
           $(this).change();
         }, FILTER_DELAY)
       );
-
-    this.resizeHandler();
-
-    window.addEventListener("resize", () => this.resizeHandler());
-
-    if (this.device.isMobile()) {
-      this.element
-        .querySelector("#search-trigger")
-        .addEventListener("click", e => {
-          e.preventDefault();
-          e.stopImmediatePropagation();
-
-          let mobile = this.device.isMobile();
-          let query = this.searchBox.val();
-
-          if (mobile && 0 == query.length) {
-            el.classList.toggle("expand-collapse");
-            el.classList.toggle("sb-search-open");
-          }
-        });
-    }
   }
 
   resizeHandler() {
@@ -70,40 +87,7 @@ export default class extends Controller {
     }
   }
 
-  handleItemKeyDown(event, item) {
-    const items = this.menu.getElementsByTagName("li");
-
-    if (!items.length) {
-      return;
-    }
-
-    switch (event.keyCode) {
-      case 9: // tab
-        return;
-      case 13: // enter
-        this.props.push(item.href); // change url
-        break;
-      case 27: // escape
-        // TODO if open closeMenu()
-        break;
-      case 38: // up
-        if (event.target === items[0]) {
-          // we're on the first item, so focus the input
-          this.props.input.focus();
-        } else {
-          event.target.previousSibling.focus();
-        }
-        break;
-      case 40: // down
-        if (event.target === items[items.length - 1]) {
-          items[0].focus();
-        } else {
-          event.target.nextSibling.focus();
-        }
-        break;
-      default:
-        return;
-    }
-    event.preventDefault();
+  doSearch(query) {
+    Turbolinks.visit(`/search?query=${query}`)
   }
 }
