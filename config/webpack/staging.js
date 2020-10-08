b581706bae3540c04b3423755bdae6e78e668a2a
Naveed Ahmad <naveedahmada036@gmail.com>
2020-10-09 03:10:04 +0500

    added staging env for webpacker

--- config/webpack/staging.js
+++ config/webpack/staging.js
@@ -1,5 +1,32 @@
-process.env.NODE_ENV = process.env.NODE_ENV || "production";
+process.env.NODE_ENV = process.env.NODE_ENV || "staging";
 
 const environment = require("./environment");
+
+const glob = require("glob-all");
+const fs = require("fs");
+const path = require("path");
+
+// Configure PurgeCSS
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+
+/*var whitelist_path = "config/webpack/purgecss/whitelist.json";
+var whitelist = {};
+
+if (fs.existsSync(whitelist_path)) {
+  whitelist = JSON.parse(fs.readFileSync(whitelist_path));
+}*/
+
+environment.plugins.append(
+  "PurgecssPlugin",
+  new PurgecssPlugin({
+    whitelist: ["tajweed", ".h", "inline-comment", ".urdu", "sup"],
+    whitelistPatterns: [/hlt/],
+    whitelistPatternsChildren: [],
+    paths: glob.sync([
+      path.join(__dirname, "../../app/javascript/**/*.js"),
+      path.join(__dirname, "../../app/views/**/*.erb")
+    ])
+  })
+);
 
 module.exports = environment.toWebpackConfig();
