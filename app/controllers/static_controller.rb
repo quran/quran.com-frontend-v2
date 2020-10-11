bbf4eb1c37d8589207610d761d584fce15300eea
Naveed Ahmad <naveedahmada036@gmail.com>
2020-10-12 02:38:29 +0500

    and more static pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class StaticController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :serviceworker
+  caches_page :opensearch,
+              :manifest,
+              :msapplication_config,
+              :serviceworker
 
   def opensearch
     render formats: :xml
