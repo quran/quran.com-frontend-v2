776eed9c5ab4ef65470f6ec28f0f5399236db98e
Naveed Ahmad <naveedahmada036@gmail.com>
2020-10-15 02:15:39 +0500

    removed noise from produciton logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,11 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  # config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  # config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
