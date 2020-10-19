746c2f5ddb50b5e5574e807286216f2448488600
Naveed Ahmad <naveedahmada036@gmail.com>
2020-10-19 23:29:21 +0500

    added tafsir for next/prev ayah

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,6 +1,11 @@
 class VersesController < ApplicationController
   before_action :init_presenter
 
+  caches_action :share,
+                :select_tafsirs,
+                :tafsir,
+                cache_path: :generate_localised_cache_key
+
   def share
     render layout: false
   end
@@ -20,5 +25,9 @@ class VersesController < ApplicationController
     unless @presenter.verse
       redirect_to '/', alert: t('errors.invalid_verse')
     end
+  end
+
+  def generate_localised_cache_key
+    "#{controller_name}/#{action_name}/#{params[:id]}-#{params[:tafsir_id]}/#{fetch_locale}"
   end
 end
