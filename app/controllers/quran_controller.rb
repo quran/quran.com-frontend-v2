3f2f41c0c32c36754b6f0b3d6aed40254e37f434
Naveed Ahmad <naveedahmada036@gmail.com>
2021-02-28 00:15:14 +0500

    init presenter before generting the cache key

--- app/controllers/quran_controller.rb
+++ app/controllers/quran_controller.rb
@@ -1,12 +1,11 @@
 class QuranController < ApplicationController
+  before_action :init_presenter
   caches_action :page,
                 :juz,
                 :juz_verses,
                 cache_path: :generate_localised_cache_key
 
   def page
-    @presenter = PagePresenter.new(self)
-
     if !@presenter.valid_page?
       return redirect_to root_path, error: t('errors.invalid_page')
     end
@@ -15,8 +14,6 @@ class QuranController < ApplicationController
   end
 
   def juz
-    @presenter = JuzPresenter.new(self)
-
     if !@presenter.valid_juz?
       return redirect_to root_path, error: t('errors.invalid_juz')
     end
@@ -26,13 +23,22 @@ class QuranController < ApplicationController
 
   def juz_verses
     params[:after] = Verse.find_by(verse_key: params[:verse]).id
-    @presenter = JuzPresenter.new(self)
 
     render layout: false
   end
 
   protected
+
   def generate_localised_cache_key
     "quran:xhr#{request.xhr?}/#{@presenter.cache_key}/#{fetch_locale}"
+  end
+
+  def init_presenter
+    case action_name
+    when 'juz', 'juz_verses'
+      @presenter = JuzPresenter.new(self)
+    when 'page'
+      @presenter = PagePresenter.new(self)
+    end
   end
 end
