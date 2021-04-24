1ea7decd0b7b8c7310fc5bb4bcda9d3b42f8d7ac
Naveed Ahmad <naveedahmada036@gmail.com>
2021-04-25 01:32:19 +0500

    fixed default

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w[.svg .eot .woff .ttf .woff2 .otf .scss]
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
